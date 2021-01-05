5fe17a58a7fc4b6b029f919ddab294d9a2a29905
dom <domcermak64@gmail.com>
2021-01-05 17:24:20 +0100

    Increase space for strings

--- db/scripts/create.sql
+++ db/scripts/create.sql
@@ -8,8 +8,8 @@ BEGIN TRY
     CREATE TABLE doctors
     (
         id      int          NOT NULL IDENTITY (100,1),
-        name    nvarchar(50) NOT NULL,
-        surname nvarchar(50) NOT NULL,
+        name    nvarchar(100) NOT NULL,
+        surname nvarchar(100) NOT NULL,
         salary  int          NULL,
         CONSTRAINT doctors_pk PRIMARY KEY (id)
     )
@@ -18,8 +18,8 @@ BEGIN TRY
     CREATE TABLE hospitals
     (
         id   int          NOT NULL IDENTITY (1,1),
-        name nvarchar(50) NOT NULL,
-        city nvarchar(50) NOT NULL,
+        name nvarchar(100) NOT NULL,
+        city nvarchar(100) NOT NULL,
         CONSTRAINT hospitals_pk PRIMARY KEY (id)
     )
 
@@ -36,8 +36,8 @@ BEGIN TRY
     CREATE TABLE patients
     (
         id           int          NOT NULL IDENTITY,
-        name         nvarchar(50) NOT NULL,
-        surname      nvarchar(50) NOT NULL,
+        name         nvarchar(100) NOT NULL,
+        surname      nvarchar(100) NOT NULL,
         birth_number bigint       NOT NULL,
         CONSTRAINT patients_birth_number UNIQUE (birth_number),
         CONSTRAINT patients_pk PRIMARY KEY (id)
@@ -59,8 +59,8 @@ BEGIN TRY
     CREATE TABLE vaccines
     (
         id           int          NOT NULL IDENTITY (10,2),
-        name         nvarchar(50) NOT NULL,
-        manufacturer nvarchar(50) NOT NULL,
+        name         nvarchar(100) NOT NULL,
+        manufacturer nvarchar(100) NOT NULL,
         price        int          NOT NULL,
         CONSTRAINT vaccines_pk PRIMARY KEY (id)
     )
