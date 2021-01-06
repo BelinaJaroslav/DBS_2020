eebbf285ead78f70672087db71a1c82adf859e6b
BelinaJaroslav <jaroslav.belina@tul.cz>
2021-01-06 20:43:07 +0100

    Update HelpTextFormatter.java

--- src/app/formatters/HelpTextFormatter.java
+++ src/app/formatters/HelpTextFormatter.java
@@ -6,7 +6,7 @@ public class HelpTextFormatter implements Formattable {
       return "Vaccination register options:\n"
             + String.format("%-110s %s\n", "list (patients | doctors | vaccines | hospitals | hospitals_doctors | registered_vaccinations) [id]", "Lists records in selected entity")
             + String.format("%-110s %s\n", "delete_patient (patient_id)", "Deletes patient and it's registrations from the registry")
-            + String.format("%-110s %s\n", "cancel_registration (patient_id)", "Deletes registered vaccination from registry")
+            + String.format("%-110s %s\n", "cancel_registration (registration_id)", "Deletes registered vaccination from registry")
             + String.format("%-110s %s\n", "exit", "Exit the program")
             + String.format("%-110s %s\n", "help", "Shows this help text");
    }
